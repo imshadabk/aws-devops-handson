f698101a4eb53c8faacb956eac2c87afdd9d35c3
shadab05 <shadabiskylar@gmail.com>
2025-11-12 11:01:49 +0000

    Files Created

---
+++ ec2.tf
@@ -0,0 +1,67 @@
+# key pair (login)
+
+resource aws_key_pair my_key {
+    key_name = "terra-key-ec2"
+    public_key = file("terra-key-ec2.pub")
+}
+
+# VPC & Security Group
+
+resource aws_default_vpc default {
+
+}
+
+resource aws_security_group my_security_group {
+    name = "automate-sg"
+    description = "this will add a TF generated security group"
+    vpc_id = aws_default_vpc.default.id
+
+    # Inbound Rules
+
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "SSH Open"
+    }
+
+    ingress {
+        from_port = 3000
+        to_port = 3000
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "HTTP Open"
+    }
+
+    # Outbound Rules
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
+        description = "all access open outbound"
+    }
+
+    tags = {
+        name = "automate-sg"
+    }
+}
+
+# ec2 Instance
+
+resource "aws_instance" "my_instance" {
+    key_name = aws_key_pair.my_key.key_name
+    security_groups = [aws_security_group.my_security_group.name]
+    instance_type = "t3.micro"
+    ami = "ami-02b8269d5e85954ef"
+
+    root_block_device  {
+        volume_size = 15
+        volume_type = "gp3"
+    }
+    tags = {
+        Name = "devops"
+    }
+}
